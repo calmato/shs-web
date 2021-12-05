5d458975c88d1055a38207c88063c9c62c657aac
nishi <tadashi-112@hotmail.co.jp>
2021-12-06 01:10:55 +0900

    feat(web/teacher): implement menu component (#1)

--- web/teacher/src/components/organisms/TheHeader.vue
+++ web/teacher/src/components/organisms/TheHeader.vue
@@ -2,7 +2,10 @@
   <v-app-bar app dark color="primary" class="px-2">
     <v-toolbar-title>SHS Web</v-toolbar-title>
     <v-spacer />
-    <v-app-bar-nav-icon @click="onClick" />
+    <v-btn icon @click="onClick">
+      <v-icon v-if="overlay">mdi-close</v-icon>
+      <v-icon v-else>mdi-menu</v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
 
@@ -10,6 +13,13 @@
 import { defineComponent, SetupContext } from '@nuxtjs/composition-api'
 
 export default defineComponent({
+  props: {
+    overlay: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
   setup(_, { emit }: SetupContext) {
     const onClick = (): void => {
       emit('click')
--- web/teacher/src/layouts/default.vue
+++ web/teacher/src/layouts/default.vue
@@ -1,28 +1,77 @@
 <template>
   <v-app>
-    <the-header @click="onClick" />
+    <the-header :overlay="overlay" @click="handleClickMenu" />
     <v-main>
       <nuxt />
+      <the-menu
+        :overlay="overlay"
+        :absolute="true"
+        :items="items"
+        @click:item="handleClickMenuItem"
+        @click:close="handleClickMenu"
+      />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref, SetupContext } from '@nuxtjs/composition-api'
 import TheHeader from '~/components/organisms/TheHeader.vue'
+import TheMenu from '~/components/organisms/TheMenu.vue'
+import { Menu } from '~/types/props/menu'
 
 export default defineComponent({
   components: {
     TheHeader,
+    TheMenu,
   },
 
-  setup() {
-    const onClick = (): void => {
-      console.log('debug', 'header')
+  setup(_, { root }: SetupContext) {
+    const router = root.$router
+
+    const items: Menu[] = [
+      {
+        name: 'トップ',
+        icon: 'mdi-home',
+        path: '/',
+      },
+      {
+        name: 'シフト希望',
+        icon: 'mdi-note-edit-outline',
+        path: '#シフト希望', // TODO: update
+      },
+      {
+        name: 'シフト作成',
+        icon: 'mdi-chart-box-plus-outline',
+        path: '#シフト作成', // TODO: update
+      },
+      {
+        name: 'ユーザ一覧',
+        icon: 'mdi-format-list-bulleted',
+        path: '#ユーザ一覧', // TODO: update
+      },
+      {
+        name: '設定',
+        icon: 'mdi-cogs',
+        path: '#設定', // TODO: update
+      },
+    ]
+
+    const overlay = ref<boolean>(false)
+
+    const handleClickMenu = (): void => {
+      overlay.value = !overlay.value
+    }
+
+    const handleClickMenuItem = (item: Menu): void => {
+      router.push(item.path)
     }
 
     return {
-      onClick,
+      items,
+      overlay,
+      handleClickMenu,
+      handleClickMenuItem,
     }
   },
 })
