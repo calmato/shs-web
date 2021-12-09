2924dcf00b6c9d3c0c61793b4c78a82a349546e7
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-12-09 20:32:55 +0900

    feat(infra): setup mysql container

---
+++ infra/mysql/schema/2021120902-users-create-table-teacher.sql
@@ -0,0 +1,22 @@
+-- -----------------------------------------------
+-- Database: users
+-- -----------------------------------------------
+CREATE SCHEMA `users` DEFAULT CHARACTER SET utf8mb4;
+
+-- -----------------------------------------------
+-- Table: teachers
+-- -----------------------------------------------
+CREATE TABLE `users`.`teachers` (
+  `id`              VARCHAR(21)  NOT NULL,             -- ユーザーID
+  `mail`            VARCHAR(256) NOT NULL,             -- メールアドレス
+  `role`            INT          NOT NULL,             -- 権限
+  `last_name`       VARCHAR(16)  NOT NULL,             -- 姓
+  `first_name`      VARCHAR(16)  NOT NULL,             -- 名
+  `last_name_kana`  VARCHAR(32)  NOT NULL,             -- 姓(かな)
+  `first_name_kana` VARCHAR(32)  NOT NULL,             -- 名(かな)
+  `created_at`      DATETIME     NOT NULL,             -- 登録日時
+  `updated_at`      DATETIME     NOT NULL,             -- 更新日時
+  `deleted_at`      DATETIME     NULL     DEFAULT NULL -- 退会日時
+) ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `mail_UNIQUE` ON `users`.`teachers` (`mail` ASC) VISIBLE;
