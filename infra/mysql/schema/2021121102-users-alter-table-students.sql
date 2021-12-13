c0505d5029e2d8150b34b2259d78a5e72e88b7aa
kaito <sea_rr10_ch@outlook.com>
2021-12-14 00:54:09 +0900

    fix: add PRYMARY KEY

---
+++ infra/mysql/schema/2021121102-users-alter-table-students.sql
@@ -0,0 +1,17 @@
+DROP TABLE `users`.`students`;
+
+CREATE TABLE `users`.`students` (
+  `id`              VARCHAR(22)  NOT NULL,             -- ユーザーID
+  `mail`            VARCHAR(256) NOT NULL,             -- メールアドレス
+  `last_name`       VARCHAR(16)  NOT NULL,             -- 姓(漢字)
+  `first_name`      VARCHAR(16)  NOT NUll,             -- 名(漢字)
+  `last_name_kana`  VARCHAR(32)  NOT NULL,             -- 姓(かな)
+  `first_name_kana` VARCHAR(32)  NOT NULL,             -- 名(かな)
+  `birth_year`      INT(4)       NOT NULL,             -- 誕生年
+  `created_at`      DATETIME     NOT NULL,             -- 登録日時
+  `updated_at`      DATETIME     NOT NULL,             -- 更新日時
+  `deleted_at`      DATETIME     NULL     DEFAULT NULL -- 退会日時
+  PRIMARY KEY(`id`)
+) ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `mail_UNIQUE` ON `users`.`students` (`mail` ASC) VISIBLE;
