24e4f81db39cc6ca9583266e011be17f91aa8ac1
nishi <tadashi-112@hotmail.co.jp>
2021-12-14 01:40:45 +0900

    feat(gateway): implement get teacher api (#24)

--- api/internal/gateway/teacher/v1/handler/teacher.go
+++ api/internal/gateway/teacher/v1/handler/teacher.go
@@ -1,13 +1,33 @@
 package handler
 
 import (
+	"context"
 	"net/http"
 
+	gentity "github.com/calmato/shs-web/api/internal/gateway/entity"
 	"github.com/calmato/shs-web/api/internal/gateway/teacher/v1/entity"
 	"github.com/calmato/shs-web/api/internal/gateway/teacher/v1/request"
 	"github.com/calmato/shs-web/api/internal/gateway/teacher/v1/response"
+	"github.com/calmato/shs-web/api/internal/gateway/util"
+	"github.com/calmato/shs-web/api/proto/user"
 	"github.com/gin-gonic/gin"
 )
+
+func (h *apiV1Handler) GetTeacher(ctx *gin.Context) {
+	c := util.SetMetadata(ctx)
+
+	teacherID := ctx.Param("teacherId")
+	teacher, err := h.getTeacher(c, teacherID)
+	if err != nil {
+		httpError(ctx, err)
+		return
+	}
+
+	res := &response.TeacherResponse{
+		Teacher: entity.NewTeacher(teacher),
+	}
+	ctx.JSON(http.StatusOK, res)
+}
 
 // mock
 func (h *apiV1Handler) CreateTeacher(ctx *gin.Context) {
@@ -33,4 +53,15 @@ func (h *apiV1Handler) CreateTeacher(ctx *gin.Context) {
 		},
 	}
 	ctx.JSON(http.StatusOK, res)
+}
+
+func (h *apiV1Handler) getTeacher(ctx context.Context, teacherID string) (*gentity.Teacher, error) {
+	in := &user.GetTeacherRequest{
+		Id: teacherID,
+	}
+	out, err := h.user.GetTeacher(ctx, in)
+	if err != nil {
+		return nil, err
+	}
+	return gentity.NewTeacher(out.Teacher), nil
 }
