61b1be93304cefce10296b0cba2fd545e4f1c87d
kaito <sea_rr10_ch@outlook.com>
2021-12-20 00:54:47 +0900

    feat: add student entity test

--- api/internal/gateway/entity/student.go
+++ api/internal/gateway/entity/student.go
@@ -1,46 +1,25 @@
 package entity
 
 import (
-	"time"
-
 	"github.com/calmato/shs-web/api/proto/user"
-	"gorm.io/gorm"
 )
 
 type Student struct {
-	ID            string         `gorm:"primaryKey;<-:create"`
-	LastName      string         `gorm:""`
-	FirstName     string         `gorm:""`
-	LastNameKana  string         `gorm:""`
-	FirstNameKana string         `gorm:""`
-	Mail          string         `gorm:""`
-	BirthYear     int64          `gorm:""`
-	Password      string         `gorm:"-"`
-	CreatedAt     time.Time      `gorm:"<-:create"`
-	UpdatedAt     time.Time      `gorm:""`
-	DeletedAt     gorm.DeletedAt `gorm:"default:null"`
+	*user.Student
 }
 
 type Students []*Student
 
-func (s *Student) Proto() *user.Student {
-	return &user.Student{
-		Id:            s.ID,
-		LastName:      s.LastName,
-		FirstName:     s.FirstName,
-		LastNameKana:  s.LastNameKana,
-		FirstNameKana: s.FirstNameKana,
-		Mail:          s.Mail,
-		BirthYear:     s.BirthYear,
-		CreatedAt:     s.CreatedAt.Unix(),
-		UpdatedAt:     s.CreatedAt.Unix(),
+func NewStudent(student *user.Student) *Student {
+	return &Student{
+		Student: student,
 	}
 }
 
-func (ss Students) Proto() []*user.Student {
-	students := make([]*user.Student, len(ss))
-	for i := range ss {
-		students[i] = ss[i].Proto()
+func NewStudents(students []*user.Student) Students {
+	ss := make(Students, len(students))
+	for i := range students {
+		ss[i] = NewStudent(students[i])
 	}
-	return students
+	return ss
 }
