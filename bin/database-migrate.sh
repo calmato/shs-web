d38f6f19f0674bdb3ce2f979ae16d159dfdef190
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-12-19 12:26:52 +0900

    chore(*): update database script

--- bin/database-migrate.sh
+++ bin/database-migrate.sh
@@ -3,6 +3,8 @@
 set -e
 
 # setup
+docker-compose stop mysql mysql_test
+docker-compose rm -vf mysql mysql_test
 docker-compose up --no-start mysql mysql_test
 docker-compose start mysql mysql_test
 docker-compose exec mysql bash -c "until mysqladmin ping -u root -p12345678; do sleep 3; done; echo 'done'"
