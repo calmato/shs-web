7a23564b7e53297dc43c7604fa79d65d374d6137
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-12-23 00:50:02 +0900

    fix(classroom-api): fix small

--- api/internal/classroom/database/teacher_subject_test.go
+++ api/internal/classroom/database/teacher_subject_test.go
@@ -174,7 +174,6 @@ func TestTeacherSubject_Replace(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
 
