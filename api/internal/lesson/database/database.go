65be8f4b3e7ad4b0ae2a74d1ba97b588cdbf748f
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-12-31 18:08:53 +0900

    fix(*): fix lint

--- api/internal/lesson/database/database.go
+++ api/internal/lesson/database/database.go
@@ -70,9 +70,9 @@ func dbError(err error) error {
 	}
 
 	//nolint:gocritic
-	switch err.(type) {
+	switch err := err.(type) {
 	case *mysql.MySQLError:
-		if err.(*mysql.MySQLError).Number == 1062 {
+		if err.Number == 1062 {
 			return fmt.Errorf("%w: %s", ErrAlreadyExists, err)
 		}
 		return fmt.Errorf("%w: %s", ErrUnknown, err)
