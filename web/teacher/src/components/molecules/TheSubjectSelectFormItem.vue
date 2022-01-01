a1d13026d856184aee2984748224088793371071
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-01-01 22:27:14 +0900

    feat: ユーザー情報変更ページの要素を配置

---
+++ web/teacher/src/components/molecules/TheSubjectSelectFormItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <the-form-group>
+    <div class="d-flex h-stack">
+      <v-text-field class="flex-grow-1" label="氏名（姓）" :value="user.lastName" readonly />
+      <v-text-field class="flex-grow-1" label="氏名（名）" :value="user.firstName" readonly />
+    </div>
+    <div class="d-flex h-stack">
+      <v-text-field class="flex-grow-1" label="氏名（姓：かな）" :value="user.lastNameKana" readonly />
+      <v-text-field class="flex-grow-1" label="氏名（名：かな）" :value="user.firstNameKana" readonly />
+    </div>
+    <v-select label="担当科目（小学校）" chips append-outer-icon="mdi-lead-pencil" />
+    <v-select label="担当科目（中学校）" chips append-outer-icon="mdi-lead-pencil" />
+    <v-select label="担当科目（高校）" chips append-outer-icon="mdi-lead-pencil" />
+  </the-form-group>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from '@nuxtjs/composition-api'
+
+interface UserPoop {
+  lastName: string
+  firstName: string
+  lastNameKana: string
+  firstNameKana: string
+}
+
+export default defineComponent({
+  components: {},
+
+  props: {
+    subjectForm: {
+      type: Object,
+      default: () => ({}),
+    },
+    user: {
+      type: Object as PropType<UserPoop>,
+      default: () => ({
+        lastName: '',
+        firstName: '',
+        lastNameKana: '',
+        firstNameKana: '',
+      }),
+    },
+  },
+
+  setup(props, { emit }) {
+    const formData = computed({
+      get: () => props.subjectForm,
+      set: (val: object) => emit('update:form', val),
+    })
+
+    return {
+      formData,
+    }
+  },
+})
+</script>
+
+<style lang="scss">
+.h-stack {
+  gap: var(--space, 1rem);
+}
+</style>
