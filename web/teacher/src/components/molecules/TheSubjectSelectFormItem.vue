ff46f5ed929e168946123fa2a7bc8e89ee699587
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-01-01 23:20:20 +0900

    feat: 科目をproprから入れられるようにした

--- web/teacher/src/components/molecules/TheSubjectSelectFormItem.vue
+++ web/teacher/src/components/molecules/TheSubjectSelectFormItem.vue
@@ -8,14 +8,36 @@
       <v-text-field class="flex-grow-1" label="氏名（姓：かな）" :value="user.lastNameKana" readonly />
       <v-text-field class="flex-grow-1" label="氏名（名：かな）" :value="user.firstNameKana" readonly />
     </div>
-    <v-select label="担当科目（小学校）" chips append-outer-icon="mdi-lead-pencil" />
-    <v-select label="担当科目（中学校）" chips append-outer-icon="mdi-lead-pencil" />
-    <v-select label="担当科目（高校）" chips append-outer-icon="mdi-lead-pencil" />
+    <v-select
+      label="担当科目（小学校）"
+      chips
+      multiple
+      append-outer-icon="mdi-lead-pencil"
+      item-text="name"
+      :items="elementarySchoolSubjects"
+    />
+    <v-select
+      label="担当科目（中学校）"
+      chips
+      multiple
+      append-outer-icon="mdi-lead-pencil"
+      item-text="name"
+      :items="juniorHighSchoolSubjects"
+    />
+    <v-select
+      label="担当科目（高校）"
+      chips
+      multiple
+      append-outer-icon="mdi-lead-pencil"
+      item-text="name"
+      :items="highSchoolSubjects"
+    />
   </the-form-group>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, PropType } from '@nuxtjs/composition-api'
+import { Subject } from '~/types/store'
 
 interface UserPoop {
   lastName: string
@@ -40,6 +62,18 @@ export default defineComponent({
         lastNameKana: '',
         firstNameKana: '',
       }),
+    },
+    elementarySchoolSubjects: {
+      type: Array as PropType<Subject[]>,
+      default: () => [],
+    },
+    juniorHighSchoolSubjects: {
+      type: Array as PropType<Subject[]>,
+      default: () => [],
+    },
+    highSchoolSubjects: {
+      type: Array as PropType<Subject[]>,
+      default: () => [],
     },
   },
 
