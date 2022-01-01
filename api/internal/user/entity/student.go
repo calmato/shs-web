abe7a9199ad93c842679c7af7dc28040bf917f8a
kaito <sea_rr10_ch@outlook.com>
2022-01-02 03:31:45 +0900

    add Fill SchoolType and Grade method

--- api/internal/user/entity/student.go
+++ api/internal/user/entity/student.go
@@ -7,6 +7,15 @@ import (
 	"gorm.io/gorm"
 )
 
+type SchoolType int32
+
+const (
+	SchoolTypeUnknown          SchoolType = 0
+	SchoolTypeElementarySchool SchoolType = 1
+	SchoolTypeJuniorHighSchool SchoolType = 2
+	SchoolTypeHighSchool       SchoolType = 3
+)
+
 type Student struct {
 	ID            string         `gorm:"primaryKey;<-:create"`
 	LastName      string         `gorm:""`
@@ -15,6 +24,8 @@ type Student struct {
 	FirstNameKana string         `gorm:""`
 	Mail          string         `gorm:""`
 	BirthYear     int64          `gorm:""`
+	Schooltype    SchoolType     `gorm:"-"`
+	Grade         int64          `gorm:"-"`
 	Password      string         `gorm:"-"`
 	CreatedAt     time.Time      `gorm:"<-:create"`
 	UpdatedAt     time.Time      `gorm:""`
@@ -22,6 +33,43 @@ type Student struct {
 }
 
 type Students []*Student
+
+func (s *Student) Fill(now time.Time) {
+	year := int64(now.Year())
+	month := int(now.Month())
+	age := year - s.BirthYear
+	if month < 4 {
+		switch {
+		case age >= 8 && age <= 13:
+			s.Schooltype = SchoolTypeElementarySchool
+			s.Grade = age - 7
+		case age >= 14 && age <= 16:
+			s.Schooltype = SchoolTypeJuniorHighSchool
+			s.Grade = age - 13
+		case age >= 17 && age <= 19:
+			s.Schooltype = SchoolTypeHighSchool
+			s.Grade = age - 16
+		default:
+			s.Schooltype = SchoolTypeUnknown
+			s.Grade = 0
+		}
+	} else {
+		switch {
+		case age >= 7 && age <= 12:
+			s.Schooltype = SchoolTypeElementarySchool
+			s.Grade = age - 6
+		case age >= 13 && age <= 15:
+			s.Schooltype = SchoolTypeJuniorHighSchool
+			s.Grade = age - 12
+		case age >= 16 && age <= 18:
+			s.Schooltype = SchoolTypeHighSchool
+			s.Grade = age - 15
+		default:
+			s.Schooltype = SchoolTypeUnknown
+			s.Grade = 0
+		}
+	}
+}
 
 func (s *Student) Proto() *user.Student {
 	return &user.Student{
@@ -32,8 +80,16 @@ func (s *Student) Proto() *user.Student {
 		FirstNameKana: s.FirstNameKana,
 		Mail:          s.Mail,
 		BirthYear:     s.BirthYear,
+		SchoolType:    user.SchoolType(s.Schooltype),
+		Grade:         s.Grade,
 		CreatedAt:     s.CreatedAt.Unix(),
 		UpdatedAt:     s.CreatedAt.Unix(),
+	}
+}
+
+func (ss Students) Fill(now time.Time) {
+	for i := range ss {
+		ss[i].Fill(now)
 	}
 }
 
--- api/internal/user/entity/student_test.go
+++ api/internal/user/entity/student_test.go
@@ -2,22 +2,116 @@ package entity
 
 import (
 	"testing"
+	"time"
 
 	"github.com/calmato/shs-web/api/pkg/jst"
 	"github.com/calmato/shs-web/api/proto/user"
 	"github.com/stretchr/testify/assert"
 )
 
+func TestStudent_Fill(t *testing.T) {
+	t.Parallel()
+	now := jst.Date(2021, 03, 01, 12, 0, 0, 0)
+	tests := []struct {
+		name         string
+		now          time.Time
+		student      *Student
+		expectSchool SchoolType
+		expectGrade  int64
+	}{
+		{
+			name: "success to school unknown",
+			now:  now,
+			student: &Student{
+				ID:            "lKEnoE6LetnMs5Byk2a7Zx",
+				LastName:      "浜田",
+				FirstName:     "直志",
+				LastNameKana:  "はまだ",
+				FirstNameKana: "ただし",
+				Mail:          "student-test002@calmato.jp",
+				BirthYear:     2014,
+				CreatedAt:     now,
+				UpdatedAt:     now,
+			},
+			expectSchool: SchoolTypeUnknown,
+			expectGrade:  0,
+		},
+		{
+			name: "success to elementary school",
+			now:  now,
+			student: &Student{
+				ID:            "lKEnoE6LetnMs5Byk2a7Zx",
+				LastName:      "浜田",
+				FirstName:     "直志",
+				LastNameKana:  "はまだ",
+				FirstNameKana: "ただし",
+				Mail:          "student-test002@calmato.jp",
+				BirthYear:     2013,
+				CreatedAt:     now,
+				UpdatedAt:     now,
+			},
+			expectSchool: SchoolTypeElementarySchool,
+			expectGrade:  1,
+		},
+		{
+			name: "success to junior high school",
+			now:  now,
+			student: &Student{
+				ID:            "lKEnoE6LetnMs5Byk2a7Zx",
+				LastName:      "浜田",
+				FirstName:     "直志",
+				LastNameKana:  "はまだ",
+				FirstNameKana: "ただし",
+				Mail:          "student-test002@calmato.jp",
+				BirthYear:     2007,
+				CreatedAt:     now,
+				UpdatedAt:     now,
+			},
+			expectSchool: SchoolTypeJuniorHighSchool,
+			expectGrade:  1,
+		},
+		{
+			name: "success to high school",
+			now:  now,
+			student: &Student{
+				ID:            "lKEnoE6LetnMs5Byk2a7Zx",
+				LastName:      "浜田",
+				FirstName:     "直志",
+				LastNameKana:  "はまだ",
+				FirstNameKana: "ただし",
+				Mail:          "student-test002@calmato.jp",
+				BirthYear:     2004,
+				CreatedAt:     now,
+				UpdatedAt:     now,
+			},
+			expectSchool: SchoolTypeHighSchool,
+			expectGrade:  1,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			tt.student.Fill(tt.now)
+			assert.Equal(t, tt.expectSchool, tt.student.Schooltype)
+			assert.Equal(t, tt.expectGrade, tt.student.Grade)
+		})
+	}
+}
+
 func TestStudent_Proto(t *testing.T) {
 	t.Parallel()
-	now := jst.Now()
+	now := jst.Date(2021, 04, 01, 12, 0, 0, 0)
 	tests := []struct {
 		name    string
+		now     time.Time
 		student *Student
 		expect  *user.Student
 	}{
 		{
 			name: "success",
+			now:  now,
 			student: &Student{
 				ID:            "lKEnoE6LetnMs5Byk2a7Zx",
 				LastName:      "浜田",
@@ -25,7 +119,9 @@ func TestStudent_Proto(t *testing.T) {
 				LastNameKana:  "はまだ",
 				FirstNameKana: "ただし",
 				Mail:          "student-test001@calmato.jp",
-				BirthYear:     2021,
+				BirthYear:     2005,
+				Schooltype:    SchoolTypeHighSchool,
+				Grade:         1,
 				CreatedAt:     now,
 				UpdatedAt:     now,
 			},
@@ -36,7 +132,9 @@ func TestStudent_Proto(t *testing.T) {
 				LastNameKana:  "はまだ",
 				FirstNameKana: "ただし",
 				Mail:          "student-test001@calmato.jp",
-				BirthYear:     2021,
+				BirthYear:     2005,
+				SchoolType:    user.SchoolType_SCHOOL_TYPE_HIGH_SCHOOL,
+				Grade:         1,
 				CreatedAt:     now.Unix(),
 				UpdatedAt:     now.Unix(),
 			},
@@ -52,11 +150,66 @@ func TestStudent_Proto(t *testing.T) {
 	}
 }
 
-func TestStudents_Proto(t *testing.T) {
+func TestStudents_Fill(t *testing.T) {
 	t.Parallel()
-	now := jst.Now()
+	now := jst.Date(2021, 04, 01, 12, 0, 0, 0)
 	tests := []struct {
 		name     string
+		now      time.Time
+		students Students
+		expect   Students
+	}{
+		{
+			name: "success",
+			now:  now,
+			students: Students{
+				{
+					ID:            "lKEnoE6LetnMs5Byk2a7Zx",
+					LastName:      "浜田",
+					FirstName:     "直志",
+					LastNameKana:  "はまだ",
+					FirstNameKana: "ただし",
+					Mail:          "student-test001@calmato.jp",
+					BirthYear:     2005,
+					Schooltype:    SchoolTypeJuniorHighSchool,
+					Grade:         2,
+					CreatedAt:     now,
+					UpdatedAt:     now,
+				},
+			},
+			expect: Students{
+				{
+					ID:            "lKEnoE6LetnMs5Byk2a7Zx",
+					LastName:      "浜田",
+					FirstName:     "直志",
+					LastNameKana:  "はまだ",
+					FirstNameKana: "ただし",
+					Mail:          "student-test001@calmato.jp",
+					BirthYear:     2005,
+					Schooltype:    SchoolTypeHighSchool,
+					Grade:         1,
+					CreatedAt:     now,
+					UpdatedAt:     now,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			tt.students.Fill(tt.now)
+			assert.Equal(t, tt.expect, tt.students)
+		})
+	}
+}
+func TestStudents_Proto(t *testing.T) {
+	t.Parallel()
+	now := jst.Date(2021, 04, 01, 12, 0, 0, 0)
+	tests := []struct {
+		name     string
+		now      time.Time
 		students Students
 		expect   []*user.Student
 	}{
@@ -70,7 +223,9 @@ func TestStudents_Proto(t *testing.T) {
 					LastNameKana:  "はまだ",
 					FirstNameKana: "ただし",
 					Mail:          "student-test001@calmato.jp",
-					BirthYear:     2021,
+					BirthYear:     2005,
+					Schooltype:    SchoolTypeHighSchool,
+					Grade:         1,
 					CreatedAt:     now,
 					UpdatedAt:     now,
 				},
@@ -83,7 +238,9 @@ func TestStudents_Proto(t *testing.T) {
 					LastNameKana:  "はまだ",
 					FirstNameKana: "ただし",
 					Mail:          "student-test001@calmato.jp",
-					BirthYear:     2021,
+					BirthYear:     2005,
+					SchoolType:    user.SchoolType_SCHOOL_TYPE_HIGH_SCHOOL,
+					Grade:         1,
 					CreatedAt:     now.Unix(),
 					UpdatedAt:     now.Unix(),
 				},
