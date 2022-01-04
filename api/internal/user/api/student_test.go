546f6ae3b414b02aa61eb9f45fb21af11eb5a917
kaito <sea_rr10_ch@outlook.com>
2022-01-05 03:42:44 +0900

    feat(user-api): add CreateStudent api

--- api/internal/user/api/student_test.go
+++ api/internal/user/api/student_test.go
@@ -3,11 +3,13 @@ package api
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/calmato/shs-web/api/internal/user/entity"
 	"github.com/calmato/shs-web/api/internal/user/validation"
 	"github.com/calmato/shs-web/api/pkg/jst"
 	"github.com/calmato/shs-web/api/proto/user"
+	"github.com/golang/mock/gomock"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/protobuf/proto"
 )
@@ -89,6 +91,71 @@ func TestGetStudent(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, testGRPC(tt.setup, tt.expect, func(ctx context.Context, service *userService) (proto.Message, error) {
 			return service.GetStudent(ctx, tt.req)
+		}))
+	}
+}
+
+func TestCreateStudent(t *testing.T) {
+	t.Parallel()
+	now := jst.Date(2021, 04, 01, 12, 0, 0, 0)
+	req := &user.CreateStudentRequest{
+		LastName:      "山田",
+		FirstName:     "太郎",
+		LastNameKana:  "やまだ",
+		FirstNameKana: "たろう",
+		Mail:          "student-test001@calamto.jp",
+		Password:      "12345678",
+		SchoolType:    user.SchoolType_SCHOOL_TYPE_HIGH_SCHOOL,
+		Grade:         1,
+	}
+
+	tests := []struct {
+		name   string
+		setup  func(ctx context.Context, t *testing.T, mocks *mocks)
+		req    *user.CreateStudentRequest
+		now    time.Time
+		expect *testResponse
+	}{
+		{
+			name: "success",
+			setup: func(ctx context.Context, t *testing.T, mocks *mocks) {
+				mocks.validator.EXPECT().CreateStudent(req).Return(nil)
+				mocks.db.Student.EXPECT().Create(ctx, gomock.Any()).Return(nil)
+			},
+			req: req,
+			now: now,
+			expect: &testResponse{
+				code: codes.OK,
+			},
+		},
+		{
+			name: "invalid arugment",
+			setup: func(ctx context.Context, t *testing.T, mocks *mocks) {
+				req := &user.CreateStudentRequest{}
+				mocks.validator.EXPECT().CreateStudent(req).Return(validation.ErrRequestValidation)
+			},
+			req: &user.CreateStudentRequest{},
+			expect: &testResponse{
+				code: codes.InvalidArgument,
+			},
+		},
+		{
+			name: "failed to create teacher",
+			setup: func(ctx context.Context, t *testing.T, mocks *mocks) {
+				mocks.validator.EXPECT().CreateStudent(req).Return(nil)
+				mocks.db.Student.EXPECT().Create(ctx, gomock.Any()).Return(errmock)
+			},
+			req: req,
+			expect: &testResponse{
+				code: codes.Internal,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, testGRPC(tt.setup, tt.expect, func(ctx context.Context, service *userService) (proto.Message, error) {
+			return service.CreateStudent(ctx, tt.req)
 		}))
 	}
 }
