6654971106aaa7c8d9eb97f0e38f95645fcf46b1
濵田広大 <hamany0326@gmail.com>
2022-01-07 02:44:37 +0900

    Add: added basic layout

--- web/teacher/src/components/templates/TheSubmissionEdit.vue
+++ web/teacher/src/components/templates/TheSubmissionEdit.vue
@@ -1,7 +1,15 @@
 <template>
-  <v-container class="pl-6">
-    <div class="text-h6">{{ items.summary.month }}月</div>
-    <the-submission-list />
+  <v-container>
+    <div class="text-h6 mt-2">{{ items.summary.year }}年 {{ items.summary.month }}月</div>
+    <v-divider class="mt-2" />
+    <v-container class="d-flex">
+      <v-row>
+        <v-col cols="2">授業日</v-col>
+        <v-col cols="10">授業希望</v-col>
+      </v-row>
+    </v-container>
+    <v-divider class="mt-1" />
+    <the-submission-list :items="items" />
   </v-container>
 </template>
 
