a2ad14fcb895f10f6a1699d664c5eeb7e012ca30
kaito <sea_rr10_ch@outlook.com>
2022-01-13 00:47:10 +0900

    feat(user-api): add ListStudents api

--- api/internal/user/api/student.go
+++ api/internal/user/api/student.go
@@ -3,10 +3,52 @@ package api
 import (
 	"context"
 
+	"github.com/calmato/shs-web/api/internal/user/database"
 	"github.com/calmato/shs-web/api/internal/user/entity"
 	"github.com/calmato/shs-web/api/pkg/jst"
 	"github.com/calmato/shs-web/api/proto/user"
+	"golang.org/x/sync/errgroup"
 )
+
+func (s *userService) listStudents(ctx context.Context, limit, offset int64) (entity.Students, int64, error) {
+	eg, ectx := errgroup.WithContext(ctx)
+	var students entity.Students
+	eg.Go(func() (err error) {
+		params := &database.ListStudentsParams{
+			Limit:  int(limit),
+			Offset: int(offset),
+		}
+		students, err = s.db.Student.List(ectx, params)
+		return
+	})
+	var total int64
+	eg.Go(func() (err error) {
+		total, err = s.db.Student.Count(ectx)
+		return
+	})
+	if err := eg.Wait(); err != nil {
+		return nil, 0, err
+	}
+
+	return students, total, nil
+}
+
+func (s *userService) ListStudents(
+	ctx context.Context, req *user.ListStudentsRequest,
+) (*user.ListStudentsResponse, error) {
+	students, total, err := s.listStudents(ctx, req.Limit, req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	res := &user.ListStudentsResponse{
+		Students: students.Proto(),
+		Total:    total,
+	}
+	if err != nil {
+		return nil, gRPCError(err)
+	}
+	return res, nil
+}
 
 func (s *userService) GetStudent(ctx context.Context, req *user.GetStudentRequest) (*user.GetStudentResponse, error) {
 	if err := s.validator.GetStudent(req); err != nil {
