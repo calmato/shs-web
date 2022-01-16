52838d51aa45dd6986a29985043529a44a9fe30e
sea-kai <76773825+sea-kai@users.noreply.github.com>
2022-01-17 01:17:08 +0900

    feat(user-api): add DeleteStudent Proto (#186)
    * feat(user-api): add DeleteStudent Proto
    
    * chore: make mockgen

--- api/mock/proto/user/service_grpc.pb.go.go
+++ api/mock/proto/user/service_grpc.pb.go.go
@@ -76,6 +76,26 @@ func (mr *MockUserServiceClientMockRecorder) CreateTeacher(ctx, in interface{}, 
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateTeacher", reflect.TypeOf((*MockUserServiceClient)(nil).CreateTeacher), varargs...)
 }
 
+// DeleteStudent mocks base method.
+func (m *MockUserServiceClient) DeleteStudent(ctx context.Context, in *user.DeleteStudentRequest, opts ...grpc.CallOption) (*user.DeleteStudentResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{ctx, in}
+	for _, a := range opts {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "DeleteStudent", varargs...)
+	ret0, _ := ret[0].(*user.DeleteStudentResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// DeleteStudent indicates an expected call of DeleteStudent.
+func (mr *MockUserServiceClientMockRecorder) DeleteStudent(ctx, in interface{}, opts ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{ctx, in}, opts...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteStudent", reflect.TypeOf((*MockUserServiceClient)(nil).DeleteStudent), varargs...)
+}
+
 // DeleteTeacher mocks base method.
 func (m *MockUserServiceClient) DeleteTeacher(ctx context.Context, in *user.DeleteTeacherRequest, opts ...grpc.CallOption) (*user.DeleteTeacherResponse, error) {
 	m.ctrl.T.Helper()
@@ -287,6 +307,21 @@ func (m *MockUserServiceServer) CreateTeacher(arg0 context.Context, arg1 *user.C
 func (mr *MockUserServiceServerMockRecorder) CreateTeacher(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateTeacher", reflect.TypeOf((*MockUserServiceServer)(nil).CreateTeacher), arg0, arg1)
+}
+
+// DeleteStudent mocks base method.
+func (m *MockUserServiceServer) DeleteStudent(arg0 context.Context, arg1 *user.DeleteStudentRequest) (*user.DeleteStudentResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "DeleteStudent", arg0, arg1)
+	ret0, _ := ret[0].(*user.DeleteStudentResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// DeleteStudent indicates an expected call of DeleteStudent.
+func (mr *MockUserServiceServerMockRecorder) DeleteStudent(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteStudent", reflect.TypeOf((*MockUserServiceServer)(nil).DeleteStudent), arg0, arg1)
 }
 
 // DeleteTeacher mocks base method.
