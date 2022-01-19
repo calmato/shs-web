442c31cfb00b7428da7fb64749cd98db298b9756
kaito <sea_rr10_ch@outlook.com>
2022-01-19 23:09:18 +0900

    feat(user-api): add DeleteStudent validator

--- api/internal/user/validation/student_test.go
+++ api/internal/user/validation/student_test.go
@@ -439,3 +439,37 @@ func TestCreateStudent(t *testing.T) {
 		})
 	}
 }
+
+func TestDeleteStudent(t *testing.T) {
+	t.Parallel()
+	validator := NewRequestValidation()
+	tests := []struct {
+		name  string
+		req   *user.DeleteStudentRequest
+		isErr bool
+	}{
+		{
+			name: "success",
+			req: &user.DeleteStudentRequest{
+				Id: "cvcTyJFfgoDQrqC1KDHbRe",
+			},
+			isErr: false,
+		},
+		{
+			name: "Id is min_len",
+			req: &user.DeleteStudentRequest{
+				Id: "",
+			},
+			isErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			err := validator.DeleteStudent(tt.req)
+			assert.Equal(t, tt.isErr, err != nil, err)
+		})
+	}
+}
