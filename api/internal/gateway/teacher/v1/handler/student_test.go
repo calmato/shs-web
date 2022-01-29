0a81f55d9db0f8081b2246db3002c23e9f8b7e24
sea-kai <76773825+sea-kai@users.noreply.github.com>
2022-01-29 14:21:46 +0900

    feat(gateway): add ListStudents mock (#229)

--- api/internal/gateway/teacher/v1/handler/student_test.go
+++ api/internal/gateway/teacher/v1/handler/student_test.go
@@ -15,6 +15,142 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
+func TestListStudents(t *testing.T) {
+	t.Parallel()
+	now := jst.Date(2021, 8, 2, 18, 30, 0, 0)
+	students := []*user.Student{
+		{
+			Id:            idmock,
+			LastName:      "中村",
+			FirstName:     "広大",
+			LastNameKana:  "なかむら",
+			FirstNameKana: "こうだい",
+			Mail:          "student-test001@calmato.jp",
+			SchoolType:    user.SchoolType_SCHOOL_TYPE_HIGH_SCHOOL,
+			CreatedAt:     now.Unix(),
+			UpdatedAt:     now.Unix(),
+		},
+	}
+	subjects := []*classroom.Subject{
+		{
+			Id:         1,
+			Name:       "国語",
+			Color:      "#F8BBD0",
+			SchoolType: classroom.SchoolType_SCHOOL_TYPE_HIGH_SCHOOL,
+			CreatedAt:  now.Unix(),
+			UpdatedAt:  now.Unix(),
+		},
+	}
+	studentSubjects := []*classroom.StudentSubject{
+		{
+			StudentId:  idmock,
+			SubjectIds: []int64{1},
+		},
+	}
+	tests := []struct {
+		name   string
+		setup  func(ctx context.Context, t *testing.T, mocks *mocks, ctrl *gomock.Controller)
+		query  string
+		expect *testResponse
+	}{
+		{
+			name: "success",
+			setup: func(ctx context.Context, t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				studentsIn := &user.ListStudentsRequest{Limit: 100, Offset: 20}
+				studentsOut := &user.ListStudentsResponse{Students: students, Total: 1}
+				subjectsIn := &classroom.MultiGetStudentSubjectsRequest{StudentIds: []string{idmock}}
+				subjectsOut := &classroom.MultiGetStudentSubjectsResponse{
+					StudentSubjects: studentSubjects,
+					Subjects:        subjects,
+				}
+				mocks.user.EXPECT().ListStudents(gomock.Any(), studentsIn).Return(studentsOut, nil)
+				mocks.classroom.EXPECT().MultiGetStudentSubjects(gomock.Any(), subjectsIn).Return(subjectsOut, nil)
+			},
+			query: "?limit=100&offset=20",
+			expect: &testResponse{
+				code: http.StatusOK,
+				body: &response.StudentsResponse{
+					Students: entity.Students{
+						{
+							ID:            idmock,
+							LastName:      "中村",
+							FirstName:     "広大",
+							LastNameKana:  "なかむら",
+							FirstNameKana: "こうだい",
+							Mail:          "student-test001@calmato.jp",
+							SchoolType:    entity.SchoolTypeHighSchool,
+							CreatedAt:     now,
+							UpdatedAt:     now,
+							Subjects: entity.Subjects{
+								{
+									ID:         1,
+									Name:       "国語",
+									Color:      "#F8BBD0",
+									SchoolType: entity.SchoolTypeHighSchool,
+									CreatedAt:  now,
+									UpdatedAt:  now,
+								},
+							},
+						},
+					},
+					Total: 1,
+				},
+			},
+		},
+		{
+			name:  "failed to invalid limit",
+			setup: func(ctx context.Context, t *testing.T, mocks *mocks, ctrl *gomock.Controller) {},
+			query: "?limit=aaa",
+			expect: &testResponse{
+				code: http.StatusBadRequest,
+			},
+		},
+		{
+			name:  "failed to invalid offset",
+			setup: func(ctx context.Context, t *testing.T, mocks *mocks, ctrl *gomock.Controller) {},
+			query: "?offset=aaa",
+			expect: &testResponse{
+				code: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "failed to list students",
+			setup: func(ctx context.Context, t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				studentsIn := &user.ListStudentsRequest{Limit: 30, Offset: 0}
+				mocks.user.EXPECT().ListStudents(gomock.Any(), studentsIn).Return(nil, errmock)
+			},
+			query: "",
+			expect: &testResponse{
+				code: http.StatusInternalServerError,
+			},
+		},
+		{
+			name: "failed to multi get subjects",
+			setup: func(ctx context.Context, t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				studentsIn := &user.ListStudentsRequest{Limit: 30, Offset: 0}
+				studentsOut := &user.ListStudentsResponse{Students: students, Total: 1}
+				subjectsIn := &classroom.MultiGetStudentSubjectsRequest{StudentIds: []string{idmock}}
+				mocks.user.EXPECT().ListStudents(gomock.Any(), studentsIn).Return(studentsOut, nil)
+				mocks.classroom.EXPECT().MultiGetStudentSubjects(gomock.Any(), subjectsIn).Return(nil, errmock)
+			},
+			query: "",
+			expect: &testResponse{
+				code: http.StatusInternalServerError,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			path := "/v1/students" + tt.query
+			req := newHTTPRequest(t, http.MethodGet, path, nil)
+			testHTTP(t, tt.setup, tt.expect, req)
+		})
+	}
+}
+
 func TestGetStudent(t *testing.T) {
 	t.Parallel()
 	now := jst.Date(2021, 8, 2, 18, 30, 0, 0)
