eae4a984bb3e85a9a197ed72b760974b91b2e071
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-01-31 00:09:47 +0900

    feat: 科目編集・削除機能の実装

--- web/teacher/src/pages/settings/subjects.vue
+++ web/teacher/src/pages/settings/subjects.vue
@@ -21,16 +21,37 @@
           <span>科目を新規作成</span>
         </v-tooltip>
       </div>
-      <v-chip v-for="subject in subjectsMap[schoolType]" :key="subject.id" :color="subject.color" class="ma-1">
+      <v-chip
+        v-for="subject in subjectsMap[schoolType]"
+        :key="subject.id"
+        :color="subject.color"
+        class="ma-1"
+        @click="handleClickSubjectChip(subject)"
+      >
         {{ subject.name }}
       </v-chip>
     </div>
 
     <v-dialog v-model="isOpen" @click:outside="handleDialogOutsideClick">
       <v-card>
-        <v-card-title class="primary white--text"> 開講科目追加（{{ formData.schoolType }}） </v-card-title>
+        <v-card-title class="primary white--text">
+          {{ `開講科目${dialogType === '新規作成' ? '追加' : '編集'}（${formData.schoolType}）` }}
+          <v-spacer />
+          <v-btn
+            v-if="dialogType === '編集'"
+            class="suffix-btn"
+            fab
+            elevation="0"
+            color="primary"
+            @click="handleDeleteButton"
+          >
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </v-card-title>
         <v-card-text class="pt-4">
-          <p class="text-body-1">追加する科目の情報を入力してください。</p>
+          <p class="text-body-1">
+            {{ `${dialogType === '新規作成' ? '追加' : '編集'}する科目の情報を入力してください。` }}
+          </p>
           <v-text-field v-model="formData.name" label="科目名"></v-text-field>
           <p class="v-label mb-0">タグの色</p>
           <v-color-picker
@@ -44,7 +65,8 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" @click="handleSubmitButton">追加</v-btn>
+          <v-btn v-if="dialogType === '新規作成'" color="primary" @click="handleSubmitButton">追加</v-btn>
+          <v-btn v-if="dialogType === '編集'" color="primary" @click="handleEditSubmitButton">編集</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -65,6 +87,9 @@ export default defineComponent({
     const swatches: string[][] = [['#F8BBD0'], ['#DBD0E6'], ['#BBDEFB'], ['#E8F5E9'], ['#FFE0B2']]
 
     const isOpen = ref<boolean>(false)
+    const dialogType = ref<'新規作成' | '編集'>('新規作成')
+
+    const selectedSubjectId = ref<number>(-1)
 
     const formData = reactive<SubjectNewForm>({
       name: '',
@@ -86,9 +111,47 @@ export default defineComponent({
       formData.color = swatches[0][0]
     }
 
+    const handleClickSubjectChip = (subject: Subject) => {
+      dialogType.value = '編集'
+      isOpen.value = true
+      selectedSubjectId.value = subject.id
+      formData.name = subject.name
+      formData.schoolType = subject.schoolType
+      formData.color = subject.color
+    }
+
     const handleAddButton = (schoolType: '小学校' | '中学校' | '高校') => {
+      dialogType.value = '新規作成'
       isOpen.value = true
       formData.schoolType = schoolType
+    }
+
+    const handleDeleteButton = async () => {
+      if (selectedSubjectId.value === -1) {
+        return
+      }
+      try {
+        await LessonStore.deleteSubject(selectedSubjectId.value)
+        selectedSubjectId.value = -1
+        isOpen.value = false
+        CommonStore.showSuccessInSnackbar(`「${formData.name}」を削除しました。`)
+      } catch (err) {
+        CommonStore.showErrorInSnackbar(err)
+      }
+    }
+
+    const handleEditSubmitButton = async () => {
+      if (selectedSubjectId.value === -1) {
+        return
+      }
+      try {
+        await LessonStore.editSubject({ ...formData, subjectId: selectedSubjectId.value })
+        selectedSubjectId.value = -1
+        isOpen.value = false
+        CommonStore.showSuccessInSnackbar(`「${formData.name}」を更新しました。`)
+      } catch (err) {
+        CommonStore.showErrorInSnackbar(err)
+      }
     }
 
     const handleSubmitButton = async () => {
@@ -107,11 +170,15 @@ export default defineComponent({
       schoolTypeArray,
       swatches,
       isOpen,
+      dialogType,
       formData,
       subjectsMap,
       handleDialogOutsideClick,
+      handleClickSubjectChip,
+      handleDeleteButton,
       handleAddButton,
       handleSubmitButton,
+      handleEditSubmitButton,
     }
   },
 })
