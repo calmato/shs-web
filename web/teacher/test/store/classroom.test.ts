1ee7750ff08c40fab4f32ae59b8de613ae5f1c96
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2022-02-05 14:04:18 +0900

    style: prettierの設定を調整

--- web/teacher/test/store/classroom.test.ts
+++ web/teacher/test/store/classroom.test.ts
@@ -1,10 +1,10 @@
-import { refresh, setSafetyMode, setup } from "~~/test//helpers/store-helper";
-import { ClassroomStore } from "~/store";
-import { ErrorResponse } from "~/types/api/exception";
-import { ApiError } from "~/types/exception";
-import { Schedule } from "~/types/api/v1";
+import { refresh, setSafetyMode, setup } from '~~/test//helpers/store-helper';
+import { ClassroomStore } from '~/store';
+import { ErrorResponse } from '~/types/api/exception';
+import { ApiError } from '~/types/exception';
+import { Schedule } from '~/types/api/v1';
 
-describe("store/classroom", () => {
+describe('store/classroom', () => {
   beforeEach(() => {
     setup();
   });
@@ -13,55 +13,55 @@ describe("store/classroom", () => {
     refresh();
   });
 
-  describe("getters", () => {
-    it("getTotalRooms", () => {
+  describe('getters', () => {
+    it('getTotalRooms', () => {
       expect(ClassroomStore.getTotalRooms).toBe(0);
     });
 
-    it("getSchedules", () => {
+    it('getSchedules', () => {
       expect(ClassroomStore.getSchedules).toEqual([]);
     });
-    it("weekdayHourFormValue", () => {
+    it('weekdayHourFormValue', () => {
       expect(ClassroomStore.weekdayHourFormValue).toEqual([]);
     });
 
-    it("holidayHourFormValue", () => {
+    it('holidayHourFormValue', () => {
       expect(ClassroomStore.holidayHourFormValue).toEqual([]);
     });
 
-    it("regularHoliday", () => {
+    it('regularHoliday', () => {
       expect(ClassroomStore.regularHoliday).toEqual([]);
     });
   });
 
-  describe("actions", () => {
-    describe("getTotalRoomsByApi", () => {
-      describe("success", () => {
+  describe('actions', () => {
+    describe('getTotalRoomsByApi', () => {
+      describe('success', () => {
         beforeEach(() => {
           setSafetyMode(true);
         });
 
-        it("return resolve", async () => {
+        it('return resolve', async () => {
           await expect(
             ClassroomStore.getTotalRoomsByApi()
           ).resolves.toBeUndefined();
         });
 
-        it("change state", async () => {
+        it('change state', async () => {
           await ClassroomStore.getTotalRoomsByApi();
           expect(ClassroomStore.getTotalRooms).toBe(5);
         });
       });
 
-      describe("failure", () => {
+      describe('failure', () => {
         beforeEach(() => {
           setSafetyMode(false);
         });
-        it("return reject", async () => {
-          const err = new ApiError(503, "api error", {
+        it('return reject', async () => {
+          const err = new ApiError(503, 'api error', {
             status: 503,
-            message: "api error",
-            details: "some error",
+            message: 'api error',
+            details: 'some error',
           } as ErrorResponse);
           try {
             await ClassroomStore.getSchedulesByApi();
@@ -72,83 +72,83 @@ describe("store/classroom", () => {
       });
     });
 
-    describe("getSchedulesByApi", () => {
-      describe("success", () => {
+    describe('getSchedulesByApi', () => {
+      describe('success', () => {
         beforeEach(() => {
           setSafetyMode(true);
         });
 
-        it("return resolve", async () => {
+        it('return resolve', async () => {
           await expect(
             ClassroomStore.getSchedulesByApi()
           ).resolves.toBeUndefined();
         });
 
-        it("change state", async () => {
+        it('change state', async () => {
           const expectedSchedules: Schedule[] = [
             {
               weekday: 0,
               isClosed: false,
               lessons: [
-                { startTime: "1530", endTime: "1700" },
-                { startTime: "1700", endTime: "1830" },
-                { startTime: "1830", endTime: "2000" },
-                { startTime: "2000", endTime: "2130" },
+                { startTime: '1530', endTime: '1700' },
+                { startTime: '1700', endTime: '1830' },
+                { startTime: '1830', endTime: '2000' },
+                { startTime: '2000', endTime: '2130' },
               ],
             },
             {
               weekday: 1,
               isClosed: false,
               lessons: [
-                { startTime: "1700", endTime: "1830" },
-                { startTime: "1830", endTime: "2000" },
-                { startTime: "2000", endTime: "2130" },
+                { startTime: '1700', endTime: '1830' },
+                { startTime: '1830', endTime: '2000' },
+                { startTime: '2000', endTime: '2130' },
               ],
             },
             {
               weekday: 2,
               isClosed: false,
               lessons: [
-                { startTime: "1700", endTime: "1830" },
-                { startTime: "1830", endTime: "2000" },
-                { startTime: "2000", endTime: "2130" },
+                { startTime: '1700', endTime: '1830' },
+                { startTime: '1830', endTime: '2000' },
+                { startTime: '2000', endTime: '2130' },
               ],
             },
             {
               weekday: 3,
               isClosed: false,
               lessons: [
-                { startTime: "1700", endTime: "1830" },
-                { startTime: "1830", endTime: "2000" },
-                { startTime: "2000", endTime: "2130" },
+                { startTime: '1700', endTime: '1830' },
+                { startTime: '1830', endTime: '2000' },
+                { startTime: '2000', endTime: '2130' },
               ],
             },
             {
               weekday: 4,
               isClosed: false,
               lessons: [
-                { startTime: "1700", endTime: "1830" },
-                { startTime: "1830", endTime: "2000" },
-                { startTime: "2000", endTime: "2130" },
+                { startTime: '1700', endTime: '1830' },
+                { startTime: '1830', endTime: '2000' },
+                { startTime: '2000', endTime: '2130' },
               ],
             },
             {
               weekday: 5,
               isClosed: false,
               lessons: [
-                { startTime: "1700", endTime: "1830" },
-                { startTime: "1830", endTime: "2000" },
-                { startTime: "2000", endTime: "2130" },
+                { startTime: '1700', endTime: '1830' },
+                { startTime: '1830', endTime: '2000' },
+                { startTime: '2000', endTime: '2130' },
               ],
             },
             {
               weekday: 6,
               isClosed: false,
               lessons: [
-                { startTime: "1530", endTime: "1700" },
-                { startTime: "1700", endTime: "1830" },
-                { startTime: "1830", endTime: "2000" },
-                { startTime: "2000", endTime: "2130" },
+                { startTime: '1530', endTime: '1700' },
+                { startTime: '1700', endTime: '1830' },
+                { startTime: '1830', endTime: '2000' },
+                { startTime: '2000', endTime: '2130' },
               ],
             },
           ];
