24e6c878e053a082b8545b3c68742b3ced7cec72
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2022-02-05 14:07:05 +0900

    style: セミコロンを消す

--- web/teacher/test/store/classroom.test.ts
+++ web/teacher/test/store/classroom.test.ts
@@ -1,88 +1,88 @@
-import { refresh, setSafetyMode, setup } from '~~/test//helpers/store-helper';
-import { ClassroomStore } from '~/store';
-import { ErrorResponse } from '~/types/api/exception';
-import { ApiError } from '~/types/exception';
-import { Schedule } from '~/types/api/v1';
+import { refresh, setSafetyMode, setup } from '~~/test//helpers/store-helper'
+import { ClassroomStore } from '~/store'
+import { ErrorResponse } from '~/types/api/exception'
+import { ApiError } from '~/types/exception'
+import { Schedule } from '~/types/api/v1'
 
 describe('store/classroom', () => {
   beforeEach(() => {
-    setup();
-  });
+    setup()
+  })
 
   afterEach(() => {
-    refresh();
-  });
+    refresh()
+  })
 
   describe('getters', () => {
     it('getTotalRooms', () => {
-      expect(ClassroomStore.getTotalRooms).toBe(0);
-    });
+      expect(ClassroomStore.getTotalRooms).toBe(0)
+    })
 
     it('getSchedules', () => {
-      expect(ClassroomStore.getSchedules).toEqual([]);
-    });
+      expect(ClassroomStore.getSchedules).toEqual([])
+    })
     it('weekdayHourFormValue', () => {
-      expect(ClassroomStore.weekdayHourFormValue).toEqual([]);
-    });
+      expect(ClassroomStore.weekdayHourFormValue).toEqual([])
+    })
 
     it('holidayHourFormValue', () => {
-      expect(ClassroomStore.holidayHourFormValue).toEqual([]);
-    });
+      expect(ClassroomStore.holidayHourFormValue).toEqual([])
+    })
 
     it('regularHoliday', () => {
-      expect(ClassroomStore.regularHoliday).toEqual([]);
-    });
-  });
+      expect(ClassroomStore.regularHoliday).toEqual([])
+    })
+  })
 
   describe('actions', () => {
     describe('getTotalRoomsByApi', () => {
       describe('success', () => {
         beforeEach(() => {
-          setSafetyMode(true);
-        });
+          setSafetyMode(true)
+        })
 
         it('return resolve', async () => {
           await expect(
             ClassroomStore.getTotalRoomsByApi()
-          ).resolves.toBeUndefined();
-        });
+          ).resolves.toBeUndefined()
+        })
 
         it('change state', async () => {
-          await ClassroomStore.getTotalRoomsByApi();
-          expect(ClassroomStore.getTotalRooms).toBe(5);
-        });
-      });
+          await ClassroomStore.getTotalRoomsByApi()
+          expect(ClassroomStore.getTotalRooms).toBe(5)
+        })
+      })
 
       describe('failure', () => {
         beforeEach(() => {
-          setSafetyMode(false);
-        });
+          setSafetyMode(false)
+        })
         it('return reject', async () => {
           const err = new ApiError(503, 'api error', {
             status: 503,
             message: 'api error',
             details: 'some error',
-          } as ErrorResponse);
+          } as ErrorResponse)
           try {
-            await ClassroomStore.getSchedulesByApi();
+            await ClassroomStore.getSchedulesByApi()
           } catch (e) {
-            expect(e).toEqual(err);
+            expect(e).toEqual(err)
           }
-        });
-      });
-    });
+        })
+      })
+    })
 
     describe('getSchedulesByApi', () => {
       describe('success', () => {
         beforeEach(() => {
-          setSafetyMode(true);
-        });
+          setSafetyMode(true)
+        })
 
         it('return resolve', async () => {
           await expect(
             ClassroomStore.getSchedulesByApi()
-          ).resolves.toBeUndefined();
-        });
+          ).resolves.toBeUndefined()
+        })
 
         it('change state', async () => {
           const expectedSchedules: Schedule[] = [
@@ -151,12 +151,12 @@ describe('store/classroom', () => {
                 { startTime: '2000', endTime: '2130' },
               ],
             },
-          ];
+          ]
 
-          await ClassroomStore.getSchedulesByApi();
-          expect(ClassroomStore.getSchedules).toEqual(expectedSchedules);
-        });
-      });
-    });
-  });
-});
+          await ClassroomStore.getSchedulesByApi()
+          expect(ClassroomStore.getSchedules).toEqual(expectedSchedules)
+        })
+      })
+    })
+  })
+})
