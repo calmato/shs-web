c5b45d5260d68e05d9a5a168055a8c3e2ef58dc8
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2022-02-05 13:56:14 +0900

    style: 不要なimportの削除

--- web/teacher/test/store/classroom.test.ts
+++ web/teacher/test/store/classroom.test.ts
@@ -1,73 +1,74 @@
-import { async } from '@firebase/util'
-import { refresh, setSafetyMode, setup } from '~~/test//helpers/store-helper'
-import { ClassroomStore } from '~/store'
-import { ErrorResponse } from '~/types/api/exception'
-import { ApiError } from '~/types/exception'
+import { refresh, setSafetyMode, setup } from "~~/test//helpers/store-helper";
+import { ClassroomStore } from "~/store";
+import { ErrorResponse } from "~/types/api/exception";
+import { ApiError } from "~/types/exception";
 
-describe('store/classroom', () => {
+describe("store/classroom", () => {
   beforeEach(() => {
-    setup()
-  })
+    setup();
+  });
 
   afterEach(() => {
-    refresh()
-  })
+    refresh();
+  });
 
-  describe('getters', () => {
-    it('getTotalRooms', () => {
-      expect(ClassroomStore.getTotalRooms).toBe(0)
-    })
+  describe("getters", () => {
+    it("getTotalRooms", () => {
+      expect(ClassroomStore.getTotalRooms).toBe(0);
+    });
 
-    it('getSchedules', () => {
-      expect(ClassroomStore.getSchedules).toEqual([])
-    })
-    it('weekdayHourFormValue', () => {
-      expect(ClassroomStore.weekdayHourFormValue).toEqual([])
-    })
+    it("getSchedules", () => {
+      expect(ClassroomStore.getSchedules).toEqual([]);
+    });
+    it("weekdayHourFormValue", () => {
+      expect(ClassroomStore.weekdayHourFormValue).toEqual([]);
+    });
 
-    it('holidayHourFormValue', () => {
-      expect(ClassroomStore.holidayHourFormValue).toEqual([])
-    })
+    it("holidayHourFormValue", () => {
+      expect(ClassroomStore.holidayHourFormValue).toEqual([]);
+    });
 
-    it('regularHoliday', () => {
-      expect(ClassroomStore.regularHoliday).toEqual([])
-    })
-  })
+    it("regularHoliday", () => {
+      expect(ClassroomStore.regularHoliday).toEqual([]);
+    });
+  });
 
-  describe('actions', () => {
-    describe('getTotalRoomsByApi', () => {
-      describe('success', () => {
+  describe("actions", () => {
+    describe("getTotalRoomsByApi", () => {
+      describe("success", () => {
         beforeEach(() => {
-          setSafetyMode(true)
-        })
+          setSafetyMode(true);
+        });
 
-        it('return resolve', async () => {
-          await expect(ClassroomStore.getTotalRoomsByApi()).resolves.toBeUndefined()
-        })
+        it("return resolve", async () => {
+          await expect(
+            ClassroomStore.getTotalRoomsByApi()
+          ).resolves.toBeUndefined();
+        });
 
-        it('change state', async () => {
-          await ClassroomStore.getTotalRoomsByApi()
-          expect(ClassroomStore.getTotalRooms).toBe(5)
-        })
-      })
+        it("change state", async () => {
+          await ClassroomStore.getTotalRoomsByApi();
+          expect(ClassroomStore.getTotalRooms).toBe(5);
+        });
+      });
 
-      describe('failure', () => {
+      describe("failure", () => {
         beforeEach(() => {
-          setSafetyMode(false)
-        })
-        it('return reject', async () => {
-          const err = new ApiError(503, 'api error', {
+          setSafetyMode(false);
+        });
+        it("return reject", async () => {
+          const err = new ApiError(503, "api error", {
             status: 503,
-            message: 'api error',
-            details: 'some error',
-          } as ErrorResponse)
+            message: "api error",
+            details: "some error",
+          } as ErrorResponse);
           try {
-            await ClassroomStore.getSchedulesByApi()
+            await ClassroomStore.getSchedulesByApi();
           } catch (e) {
-            expect(e).toEqual(err)
+            expect(e).toEqual(err);
           }
-        })
-      })
-    })
-  })
-})
+        });
+      });
+    });
+  });
+});
