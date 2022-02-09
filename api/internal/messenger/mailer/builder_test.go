c247a3dc875c8c79d7fa37e2d2607a5895589999
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2022-02-09 23:48:11 +0900

    fix(messenger-api): fix small

--- api/internal/messenger/mailer/builder_test.go
+++ api/internal/messenger/mailer/builder_test.go
@@ -12,13 +12,15 @@ func TestTemplateBuilder(t *testing.T) {
 		Data(map[string]string{"key": "value"}).
 		YearMonth(jst.Date(2022, 1, 2, 18, 30, 0, 0)).
 		Name("中村 広大").
+		WebURL("http://example.com").
 		TeacherID("teacherid").
 		StudentID("studentid")
 	data := builder.Build()
 	assert.Equal(t, "value", data["key"])
-	assert.Equal(t, "2022年01月", data["yearMonth"])
-	assert.Equal(t, "中村 広大", data["name"])
-	assert.Equal(t, "生徒", data["actor"])
+	assert.Equal(t, "2022年01月", data["年月"])
+	assert.Equal(t, "中村 広大", data["氏名"])
+	assert.Equal(t, "http://example.com", data["サイトURL"])
+	assert.Equal(t, "student", data["actor"])
 	assert.Equal(t, "teacherid", data["teacherId"])
 	assert.Equal(t, "studentid", data["studentId"])
 }
