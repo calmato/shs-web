cd7cec98ca6a4143eef40b37658d7983ec56c391
koudai hamada <hamany0326@gmail.com>
2022-02-11 23:33:47 +0900

    Add: added setting top page (#292)

---
+++ web/student/src/components/templates/TheSettingTop.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container>
+    <the-setting-user-form-item :user="user" />
+    <v-row class="py-4">
+      <v-col cols="12">
+        <v-card
+          v-for="item in menuItems"
+          :key="`menu-${item.title}`"
+          elevation="0"
+          class="my-1"
+          outlined
+          @click="onClick(item)"
+        >
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title :class="`${item.textColor}--text`">{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-icon>mdi-chevron-right</v-icon>
+            </v-list-item-action>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, SetupContext } from '@nuxtjs/composition-api'
+import TheSettingUserFormItem from '~/components/molecules/TheSettingUserFormItem.vue'
+import { Menu, UserProps } from '~/types/props/setting'
+
+export default defineComponent({
+  components: {
+    TheSettingUserFormItem,
+  },
+
+  props: {
+    menuItems: {
+      type: Array as PropType<Menu[]>,
+      default: () => [],
+    },
+    user: {
+      type: Object as PropType<UserProps>,
+      default: () => ({
+        lastName: '',
+        firstName: '',
+        lastNameKana: '',
+        firstNameKana: '',
+      }),
+    },
+  },
+
+  setup(_, { emit }: SetupContext) {
+    const onClick = (item: Menu): void => {
+      emit('click', item)
+    }
+
+    return {
+      onClick,
+    }
+  },
+})
+</script>
