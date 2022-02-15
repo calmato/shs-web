749c477ed3e6e047c52b723a22fe1e4a40d39ee2
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-02-16 01:17:49 +0900

    fix: ロケールの設定をjaに変更

--- web/student/nuxt.config.js
+++ web/student/nuxt.config.js
@@ -1,4 +1,5 @@
 import colors from 'vuetify/es5/util/colors'
+import ja from 'vuetify/lib/locale/ja'
 
 export default {
   srcDir: 'src',
@@ -79,6 +80,10 @@ export default {
       },
       options: {
         customProperties: true,
+      },
+      lang: {
+        locales: { ja },
+        current: 'ja',
       },
     },
   },
