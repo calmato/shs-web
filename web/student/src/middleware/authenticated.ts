5c6d8dd2ee38461547d298059f2392056e686d8a
nishi <ta.g-h-r.1260@docomo.ne.jp>
2022-02-16 01:24:22 +0900

    feat(web/student): update middleware logic (#316)

--- web/student/src/middleware/authenticated.ts
+++ web/student/src/middleware/authenticated.ts
@@ -1,17 +1,25 @@
 import { Context } from '@nuxt/types'
+import { InitialState } from '~/types/store'
 
 const excludedPaths: string[] = ['/signin']
 
 export default async ({ route, store, redirect }: Context) => {
   if (excludedPaths.includes(route.path)) {
+    store.dispatch('common/factory')
     return
   }
 
   await store
     .dispatch('auth/authentication')
-    .then(async () => {
-      await store.dispatch('auth/showAuth')
-      await store.dispatch('lesson/getAllSubjects')
+    .then(() => {
+      const state = store.getters['common/getInitialState']
+      if (state === InitialState.FINISHED) {
+        return
+      }
+
+      store.dispatch('common/startInitialize')
+      Promise.all([store.dispatch('auth/showAuth'), store.dispatch('lesson/getAllSubjects')])
+      store.dispatch('common/endInitialize')
     })
     .catch(() => {
       redirect('/signin')
